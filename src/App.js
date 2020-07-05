5988b3f619abbc1f049c878b9da78b5d05d71b7e
Samson Okubanjo <samsonokubanjo@outlook.com>
2020-07-05 02:45:05 +0100

    removed tailwindcss

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
-import './styles/main.css';
 
 function App() {
   return (
