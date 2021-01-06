bcc848be1d386f3626f2d74f5075483853555b5b
Samson Okubanjo <samsonokubanjo@outlook.com>
2021-01-06 03:54:27 +0100

    pre npm eject

--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -11,13 +11,13 @@ export const counterSlice = createSlice({
       // doesn't actually mutate the state because it uses the Immer library,
       // which detects changes to a "draft state" and produces a brand new
       // immutable state based off those changes
-      state.value += 1;
+      state.value += 1; // eslint-disable-line no-param-reassign
     },
     decrement: (state) => {
-      state.value -= 1;
+      state.value -= 1; // eslint-disable-line no-param-reassign
     },
     incrementByAmount: (state, action) => {
-      state.value += action.payload;
+      state.value += action.payload; // eslint-disable-line no-param-reassign
     },
   },
 });
